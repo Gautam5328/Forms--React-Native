3be50a3c95b786d8c5236018c8ad9bf6eb9868cf
Gautam Kothari <kotharigautam69@gmail.com>
2023-06-14 00:15:52 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,51 @@
+export const radioButtonData=[
+{
+  id: '1', // acts as primary key, should be unique and non-empty string
+  label: 'Male',
+  value: 'Male',
+},
+{
+  id: '2',
+  label: 'Female',
+  value: 'Female',
+},
+]
+
+
+export const  zipCodesData = [
+    {
+      zipcode: '110011',
+      city: 'Central Delhi',
+      state: 'ND',
+    },
+    {
+      zipcode: '400011',
+      city: 'Mumbai',
+      state: 'MH',
+    },
+    {
+      zipcode: '600001',
+      city: 'Chennai',
+      state: 'TN',
+    },
+    {
+      zipcode: '700001',
+      city: 'Kolkata',
+      state: 'WB',
+    },
+  ];
+
+  export const  colorsData = [
+    {name:'aliceblue', color:'#f0f8ff'},
+    {name:'antiquewhite',color: '#faebd7'},
+    {name:'aqua',color: '#00ffff'},
+    {name:'aquamarine',color: '#7fffd4'},
+    {name:'azure',color: '#f0ffff'},
+    {name:'beige',color: '#f5f5dc'},
+    {name:'bisque',color: '#ffe4c4'},
+    {name:'black',color: '#000000'},
+    {name:'blanchedalmond',color: '#ffebcd'},
+    {name:'blue',color: '#0000ff'},
+    {name:'blueviolet',color: '#8a2be2'},
+    {name:'brown',color: '#a52a2a'},
+];
